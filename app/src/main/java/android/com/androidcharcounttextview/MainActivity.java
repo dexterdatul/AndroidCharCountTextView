d1062721106cb66b0dc63eab2bea9db0832ba07e
Dexter John Datul <ddatul@almadaclinic.com>
2017-09-13 21:13:29 +0300

    Android Character Count

---
+++ app/src/main/java/android/com/androidcharcounttextview/MainActivity.java
@@ -0,0 +1,27 @@
+package android.com.androidcharcounttextview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.wafflecopter.charcounttextview.CharCountTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CharCountTextView charCountTextView = (CharCountTextView) findViewById(R.id.tvTextCounter);
+        EditText editText = (EditText) findViewById(R.id.eTxt);
+
+        charCountTextView.setEditText(editText);
+        charCountTextView.setCharCountChangedListener(new CharCountTextView.CharCountChangedListener() {
+            @Override
+            public void onCountChanged(int i, boolean b) {
+
+            }
+        });
+    }
+}
